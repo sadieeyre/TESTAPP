6c64dc93e97a737a5afc6733dfed4b45a50b6a98
sadieeyre <sadiereyre@gmail.com>
2024-03-08 18:43:08 -0700

    showing college teams

--- mission09/src/App.tsx
+++ mission09/src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import yourData from './CollegeBasketballTeams.json';
 
-function App() {
+const teamNames = yourData.teams;
 
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to March Madness!</h1>
+      <p>
+        Below are the details of each college basketball team. Feel free to
+        study up as March Madness season is upon us!
+      </p>
     </div>
   );
 }
 
+// Function to represent a team card
+
+class Teams extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h3>{oneTeam.school}</h3>
+        <p>Mascot: {oneTeam.name}</p>
+        <p>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </p>
+      </div>
+    );
+  }
+}
+
+// Function to display a list of team cards
+function TeamList() {
+  return (
+    <div className="team-list">
+      {teamNames.map((team) => (
+        <Teams {...team} />
+      ))}
+    </div>
+  );
+}
+
+//Show the welcome and the team list
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <TeamList />
+    </div>
+  );
+}
+
+//teamData={teamData}
 export default App;
